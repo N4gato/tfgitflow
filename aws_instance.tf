54eba7ffd62411d7ca344368e5982fd8f6dbcbda
Otmane BOUAYAD <otmane.bouayad@webhelp.com>
2021-07-12 11:36:40 +0200

    SR36775 - for new aws instance

--- aws_instance.tf
+++ aws_instance.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "web1" {
   ami           = "ami-a1b2c3d4"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "web2" {
+  ami           = "ami-a1b2c3d4"
+  instance_type = "t2.micro"
+}
